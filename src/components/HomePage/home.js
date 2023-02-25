08f9d115cfad69dada0292856816dc91a6839562
DspSeven <119715565+DspSeven@users.noreply.github.com>
2023-02-25 09:11:32 +0530

    Create home.js

---
+++ src/components/HomePage/home.js
@@ -0,0 +1,12 @@
+import {Component} from 'react'
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>hi</h1>
+      </div>
+    )
+  }
+}
+export default HomePage
