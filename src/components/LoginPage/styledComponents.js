41fa8a36d5e5b970856bc3260ac91cb3a12be78e
DspSeven <119715565+DspSeven@users.noreply.github.com>
2023-02-25 09:12:17 +0530

    Update styledComponents.js

--- src/components/LoginPage/styledComponents.js
+++ src/components/LoginPage/styledComponents.js
@@ -11,7 +11,36 @@ export const CustomContainer = Styled.div`
 display: flex;
 flex-direction: column;
 justify-content: center;
-align-items: center;`
+align-items: center;
+box-shadow: 0px 0px 7px;
+border: 1px solid #ffffff;
+border-radius: 7px;
+padding: 30px;
+height: 350px;`
 
 export const CustomParagraph = Styled.p`
-color:  #ff0000`
+color:  #ff0000;`
+
+export const CustomLabel = Styled.label`
+align-self: flex-start;
+padding-top: 10px;`
+
+export const CustomInput = Styled.input`
+align-self: flex-start;
+margin-top: 10px;
+height: 5vh;
+width: 95%;
+`
+export const CheckboxContainer = Styled.div`
+display: flex;
+align-self: flex-start;
+margin-top: 10px;`
+
+export const CustomButton = Styled.button`
+color: #ffffff;
+background-color:  #3b82f6;
+margin-top: 10px;
+width: 95%;
+padding: 10px;
+border: 0px solid #ffffff;
+border-radius: 7px;`
