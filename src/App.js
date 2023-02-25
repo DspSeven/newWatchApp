f202781a5f994c9f2bd7c60a194a3009a5884347
DspSeven <119715565+DspSeven@users.noreply.github.com>
2023-02-25 09:14:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import {Component} from 'react'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import NxtWatch from './context/NxtWatch/nxtWatchContext'
 import LoginPage from './components/LoginPage/login'
+import HomePage from './components/HomePage/home'
+import ProtectedRoute from './components/ProtectedRoute/protectedRoute'
 import './App.css'
 
-// Replace your code here
-const App = () => <LoginPage />
+// Replace your cod<LoginPage />
+class App extends Component {
+  state = {
+    toggleColor: '',
+  }
+
+  render() {
+    const {toggleColor} = this.state
+    return (
+      <BrowserRouter>
+        <NxtWatch.Provider value={toggleColor}>
+          <Switch>
+            <Route exact path="/login" component={LoginPage} />
+            <ProtectedRoute exact path="/" component={HomePage} />
+          </Switch>
+        </NxtWatch.Provider>
+      </BrowserRouter>
+    )
+  }
+}
 
 export default App
