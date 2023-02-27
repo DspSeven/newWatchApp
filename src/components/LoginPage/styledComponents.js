d5486aac0a7881134b3ef2a75c735a928253376c
DspSeven <119715565+DspSeven@users.noreply.github.com>
2023-02-27 15:21:40 +0530

    Update styledComponents.js

--- src/components/LoginPage/styledComponents.js
+++ src/components/LoginPage/styledComponents.js
@@ -5,7 +5,8 @@ display: flex;
 flex-direction: column;
 justify-content: center;
 align-items: center;
-height: 100vh;`
+height: 100vh;
+background-color: ${props => (props.bgColor ? '#212121' : '#ffffff')};`
 
 export const CustomContainer = Styled.div`
 display: flex;
@@ -23,7 +24,8 @@ color:  #ff0000;`
 
 export const CustomLabel = Styled.label`
 align-self: flex-start;
-padding-top: 10px;`
+padding-top: ${props => (props.sp ? null : 10)}px;
+color: ${props => (props.label ? '#ffffff' : '#000000')};`
 
 export const CustomInput = Styled.input`
 align-self: flex-start;
