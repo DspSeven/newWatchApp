313ae05fd01c3bef7e4da3d591aab38b5078b1e7
DspSeven <119715565+DspSeven@users.noreply.github.com>
2023-02-27 09:41:59 +0530

    Update styledComponents.js

--- src/components/HeaderPage/styledComponents.js
+++ src/components/HeaderPage/styledComponents.js
@@ -18,12 +18,12 @@ align-self: center;
 padding: 10px;`
 
 export const ProfileImage = Styled.img`
-height: 45px;
-width: 45px;
+height: 30px;
+width: 30px;
 margin-right: 25px;`
 
 export const HeaderButton = Styled.button`
-height: 45px;
+height: 30px;
 width: 100px;
 margin-right: 25px;
 align-self: center;
@@ -31,3 +31,32 @@ border: 1px solid ${props => (props.outline ? '#ffffff' : '#3b82f6')};
 background-color: transparent;
 color: ${props => (props.outline ? '#ffffff' : '#3b82f6')};
 `
+
+export const PopupWindow = Styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+border: 0px solid;
+border-radius: 7px;
+background-color: #212121;
+padding: 25px;`
+
+export const PopupPara = Styled.p`
+color: #ffffff;
+font-family: "Roboto";
+font-size: 25px;`
+
+export const SubmitButtons = Styled.div`
+display:flex;
+justify-content: space-between;
+align-items: center;`
+
+export const DualButton = Styled.button`
+color: #ffffff;
+background-color: ${props => (props.confirm ? '#3b82f6' : 'transparent')};
+border: ${props => (props.confirm ? 0 : 1)}px solid ${props =>
+  props.confirm ? null : '#ffffff'};
+border-radius: 7px;
+padding: 10px;
+margin-right: ${props => (props.confirm ? null : 25)}px;`
