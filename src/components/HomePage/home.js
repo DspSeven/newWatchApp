3c7346d25aaa9680964757d11c6c873acc12e659
DspSeven <119715565+DspSeven@users.noreply.github.com>
2023-02-28 17:07:24 +0530

    Update home.js

--- src/components/HomePage/home.js
+++ src/components/HomePage/home.js
@@ -1,11 +1,18 @@
 import {Component} from 'react'
 import HeaderPage from '../HeaderPage/header'
+import FilterGroup from '../FilterGroup/filter'
+import ProductsGroup from '../ProductsGroup/products'
+import {Home} from './styledComponents'
 
 class HomePage extends Component {
   render() {
     return (
       <div>
         <HeaderPage />
+        <Home>
+          <FilterGroup />
+          <ProductsGroup />
+        </Home>
       </div>
     )
   }
