9e332e5c83dca1fc3319f47434c5758c7e10fbe5
DspSeven <119715565+DspSeven@users.noreply.github.com>
2023-03-03 09:20:47 +0530

    Create styledComponents.js

---
+++ src/components/HomeRoute/styledComponents.js
@@ -0,0 +1,37 @@
+import Styled from 'styled-components'
+
+export const HomeContainer = Styled.div`
+display: flex;
+justify-content: center;
+align-items: flex-start;
+background-image: url('https://assets.ccbp.in/frontend/react-js/nxt-watch-banner-bg.png');
+background-size: cover;
+width: 100%;
+padding-right: 650px;
+background-color: ${props => (props.homeColor ? '#0f0f0f' : '#f9f9f9')};
+`
+
+export const HomeRouteContainer = Styled.div`
+background-color: ${props => (props.homeColor ? '#0f0f0f' : '#f9f9f9')};
+height: 90vh;
+overflow: scroll;
+`
+export const HomeInfo = Styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: flex-start;
+`
+export const SpanElement = Styled.span`
+`
+export const VideoContainer = Styled.div`
+display: flex;
+flex-direction: column;
+justify-content: flex-start;
+align-items: flex-start;`
+
+export const InputContainer = Styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
+padding-top: 25px;`
