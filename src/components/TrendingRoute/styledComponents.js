785bf39a57c568c0830796af5c46eae033cb24d4
DspSeven <119715565+DspSeven@users.noreply.github.com>
2023-03-06 16:29:50 +0530

    Update styledComponents.js

--- src/components/TrendingRoute/styledComponents.js
+++ src/components/TrendingRoute/styledComponents.js
@@ -7,5 +7,9 @@ export const Videos = Styled.ul`
 height: 90vh;
 overflow: scroll;`
 
-export const UnorderedList = Styled.ul`
-width: 80%;`
+export const UnorderedList = Styled.div`
+width: 75vw;`
+
+export const TrendUnorderedList = Styled.ul`
+display: flex;
+flex-direction: column;`
