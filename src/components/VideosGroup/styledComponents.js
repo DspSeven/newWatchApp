74a87e53be150727722a927915548c43fcaf6916
DspSeven <119715565+DspSeven@users.noreply.github.com>
2023-03-06 16:31:15 +0530

    Create styledComponents.js

---
+++ src/components/VideosGroup/styledComponents.js
@@ -0,0 +1,23 @@
+import Styled from 'styled-components'
+
+export const HomeVideoContainer = Styled.li`
+display: flex;
+flex-direction: column;
+height: 500px;
+width: 30%;
+border: 0px solid #ffffff;
+border-radius: 7px;
+padding-left: 25px;
+line-height: 25px;
+`
+export const PlayerInfo = Styled.div`
+display: flex;
+justify-content: space-between;
+padding-top: 25px;`
+
+export const ThumbnailImage = Styled.img`
+height: 25vh;
+width: 100%;`
+
+export const Heading = Styled.h1`
+font-size: 15px;`
