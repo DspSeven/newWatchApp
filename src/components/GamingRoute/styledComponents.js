1e5a6c44a7729a17ba22edb73629531e2912b892
DspSeven <119715565+DspSeven@users.noreply.github.com>
2023-03-06 10:04:53 +0530

    Create styledComponents.js

---
+++ src/components/GamingRoute/styledComponents.js
@@ -0,0 +1,11 @@
+import Styled from 'styled-components'
+
+export const CombineContainer = Styled.div`
+display: flex;`
+
+export const UnorderedList = Styled.ul`
+display: flex;
+flex-wrap: wrap;
+width: 80%;
+height: 75vh;
+overflow: scroll;`
