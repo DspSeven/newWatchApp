a70e24e835512af5edc9e8fb66cf7b82cd4f0f98
DspSeven <119715565+DspSeven@users.noreply.github.com>
2023-03-06 16:30:34 +0530

    Create styledComponents.js

---
+++ src/components/TrendingVideos/styledComponents.js
@@ -0,0 +1,21 @@
+import Styled from 'styled-components'
+
+export const TrendList = Styled.li`
+display: flex;
+border: 0px solid #ffffff;
+border-radius: 7px;
+height: 300px;
+width: 1500px;
+`
+export const ThumbnailImage = Styled.img`
+height: 35vh;
+width: 35vw;
+margin-left: 25px;
+padding-left: 25px;`
+
+export const TrendingInfo = Styled.div`
+padding-left: 25px;
+width: 500px;`
+
+export const Heading = Styled.h1`
+font-size: 15px;`
