f1be72a3f9dd5df7c74c77cfffb427ff5603f840
DspSeven <119715565+DspSeven@users.noreply.github.com>
2023-03-10 17:09:44 +0530

    Update gamingVideosSection.js

--- src/components/GamingVideosSection/gamingVideosSection.js
+++ src/components/GamingVideosSection/gamingVideosSection.js
@@ -1,3 +1,4 @@
+import {Link} from 'react-router-dom'
 import {ListContainer} from './styledComponents'
 
 const GamingVideosSection = props => {
@@ -5,9 +6,11 @@ const GamingVideosSection = props => {
   const {id, thumbnailUrl, title, viewCount} = gd
   return (
     <ListContainer>
-      <img src={thumbnailUrl} alt={title} height={350} width={300} />
-      <p>{title}</p>
-      <p>{viewCount} Watching Worldwide</p>
+      <Link to={`/videos/${id}`}>
+        <img src={thumbnailUrl} alt={title} height={350} width={300} />
+        <p>{title}</p>
+        <p>{viewCount} Watching Worldwide</p>
+      </Link>
     </ListContainer>
   )
 }
