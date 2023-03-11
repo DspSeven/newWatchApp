94dc00bf920eeed93db8691abbe51754db372fe5
DspSeven <119715565+DspSeven@users.noreply.github.com>
2023-03-11 16:22:59 +0530

    Create styledComponents.js

---
+++ src/components/SavedVideosSection/styledComponents.js
@@ -0,0 +1,16 @@
+import Styled from 'styled-components'
+
+export const SvsContainer = Styled.div`
+display: flex;
+justify-content: space-between;
+overflow: scroll;
+height: 90vh;`
+
+export const SvsSubContainer = Styled.div`
+display: flex;
+flex-direction: column;`
+
+export const Image = Styled.img`
+height: 300px;
+width: 300px;
+padding-right: 30px;`
