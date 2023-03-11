86910412a497003e2ebf2b0b92165d1d65205e19
DspSeven <119715565+DspSeven@users.noreply.github.com>
2023-03-11 09:22:54 +0530

    Update trendingVideos.js

--- src/components/TrendingVideos/trendingVideos.js
+++ src/components/TrendingVideos/trendingVideos.js
@@ -1,4 +1,5 @@
 import {formatDistanceToNow} from 'date-fns'
+import {Link} from 'react-router-dom'
 import {
   TrendList,
   ThumbnailImage,
@@ -8,21 +9,23 @@ import {
 
 const TrendingVideos = props => {
   const {trendingData} = props
-  const {thumbnailUrl, title, name, viewCount, publishedAt} = trendingData
+  const {id, thumbnailUrl, title, name, viewCount, publishedAt} = trendingData
   const year = new Date(publishedAt).getFullYear()
   const month = new Date(publishedAt).getMonth()
-  const date = new Date(publishedAt).getFullYear()
+  const date = new Date(publishedAt).getDate()
   const newDate = formatDistanceToNow(new Date(year, month, date))
   return (
-    <TrendList>
-      <ThumbnailImage src={thumbnailUrl} alt="url" />
-      <TrendingInfo>
-        <Heading>{title}</Heading>
-        <p>{name}</p>
-        <p>{viewCount} views</p>
-        <p>{newDate} ago</p>
-      </TrendingInfo>
-    </TrendList>
+    <Link to={`/videos/${id}`}>
+      <TrendList>
+        <ThumbnailImage src={thumbnailUrl} alt="url" />
+        <TrendingInfo>
+          <Heading>{title}</Heading>
+          <p>{name}</p>
+          <p>{viewCount} views</p>
+          <p>{newDate} ago</p>
+        </TrendingInfo>
+      </TrendList>
+    </Link>
   )
 }
 export default TrendingVideos
