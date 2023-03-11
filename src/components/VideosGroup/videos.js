cf8940cc9d54fb11229d0aadc9f01082061c2e90
DspSeven <119715565+DspSeven@users.noreply.github.com>
2023-03-11 09:22:28 +0530

    Update videos.js

--- src/components/VideosGroup/videos.js
+++ src/components/VideosGroup/videos.js
@@ -1,4 +1,5 @@
 import {formatDistanceToNow} from 'date-fns'
+import {Link} from 'react-router-dom'
 import {
   HomeVideoContainer,
   PlayerInfo,
@@ -10,23 +11,28 @@ const VideosGroup = props => {
   const {videos} = props
   const {id, publishedAt, thumbnailUrl, title, viewCount, channel} = videos
   const {name, profileImageUrl} = channel
+  const year = new Date(publishedAt).getFullYear()
   const month = new Date(publishedAt).getMonth()
+  const date = new Date(publishedAt).getDate()
   console.log(month)
-  const newDate = formatDistanceToNow(new Date(publishedAt))
+  const newDate = formatDistanceToNow(new Date(year, month, date))
   console.log(newDate)
   return (
     <HomeVideoContainer>
-      <ThumbnailImage src={thumbnailUrl} alt={title} />
-      <PlayerInfo>
-        <img src={profileImageUrl} alt={name} height={60} width={60} />
-        <div>
-          <Heading>{title}</Heading>
-          <p>{name}</p>
+      <Link to={`/videos/${id}`}>
+        <ThumbnailImage src={thumbnailUrl} alt={title} />
+        <PlayerInfo>
+          <img src={profileImageUrl} alt={name} height={60} width={60} />
           <div>
-            <p>{viewCount} views</p>
+            <Heading>{title}</Heading>
+            <p>{name}</p>
+            <div>
+              <p>{viewCount} views</p>
+              <p>{newDate} ago</p>
+            </div>
           </div>
-        </div>
-      </PlayerInfo>
+        </PlayerInfo>
+      </Link>
     </HomeVideoContainer>
   )
 }
