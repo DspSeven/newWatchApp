0d7cce2d2964af582f4f7946c14c3fbcb01168b2
DspSeven <119715565+DspSeven@users.noreply.github.com>
2023-03-11 16:22:10 +0530

    Create styledComponents.js

---
+++ src/components/SavedVideosRoute/styledComponents.js
@@ -0,0 +1,12 @@
+import Styled from 'styled-components'
+
+export const CombineContainer = Styled.div`
+display: flex;`
+
+export const NoRecords = Styled.div`
+height: 50vh;
+width: 50vw;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;`
