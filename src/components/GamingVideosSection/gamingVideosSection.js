aa3c829393d022df040b9cef870dd1cbdba0c335
DspSeven <119715565+DspSeven@users.noreply.github.com>
2023-03-13 12:27:21 +0530

    Update gamingVideosSection.js

--- src/components/GamingVideosSection/gamingVideosSection.js
+++ src/components/GamingVideosSection/gamingVideosSection.js
@@ -1,17 +1,34 @@
 import {Link} from 'react-router-dom'
+import NxtWatch from '../../context/NxtWatch/nxtWatchContext'
 import {ListContainer} from './styledComponents'
 
 const GamingVideosSection = props => {
   const {gd} = props
   const {id, thumbnailUrl, title, viewCount} = gd
+
   return (
-    <ListContainer>
-      <Link to={`/videos/${id}`}>
-        <img src={thumbnailUrl} alt={title} height={350} width={300} />
-        <p>{title}</p>
-        <p>{viewCount} Watching Worldwide</p>
-      </Link>
-    </ListContainer>
+    <NxtWatch.Consumer>
+      {value => {
+        const {changeOption} = value
+        const navToVideoInfo = () => {
+          changeOption('Job')
+        }
+        return (
+          <ListContainer onClick={navToVideoInfo}>
+            <Link to={`/videos/${id}`}>
+              <img
+                src={thumbnailUrl}
+                alt="video thumbnail"
+                height={350}
+                width={300}
+              />
+              <p>{title}</p>
+              <p>{viewCount} Watching Worldwide</p>
+            </Link>
+          </ListContainer>
+        )
+      }}
+    </NxtWatch.Consumer>
   )
 }
 export default GamingVideosSection
