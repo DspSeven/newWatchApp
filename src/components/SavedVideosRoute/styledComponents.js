50abc4f0e54b8970d45b169ddfd7ef73a9fb7191
DspSeven <119715565+DspSeven@users.noreply.github.com>
2023-03-13 12:32:10 +0530

    Update styledComponents.js

--- src/components/SavedVideosRoute/styledComponents.js
+++ src/components/SavedVideosRoute/styledComponents.js
@@ -3,7 +3,7 @@ import Styled from 'styled-components'
 export const CombineContainer = Styled.div`
 display: flex;`
 
-export const NoRecords = Styled.div`
+export const NoRecords = Styled.li`
 height: 50vh;
 width: 50vw;
 display: flex;
