718ff9cbe863b6fc3524b819e21c0efe12814fee
DspSeven <119715565+DspSeven@users.noreply.github.com>
2023-03-13 17:17:32 +0530

    Update styledComponents.js

--- src/components/TrendingRoute/styledComponents.js
+++ src/components/TrendingRoute/styledComponents.js
@@ -13,3 +13,6 @@ width: 75vw;`
 export const TrendUnorderedList = Styled.ul`
 display: flex;
 flex-direction: column;`
+
+export const TC = Styled.div`
+background-color: ${props => (props.colorChange ? '#0f0f0f' : '#f9f9f9')};`
