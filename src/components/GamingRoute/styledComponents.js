4e4e0ab43e8123de931af61eaf7f6b99b5955574
DspSeven <119715565+DspSeven@users.noreply.github.com>
2023-03-13 12:24:58 +0530

    Update styledComponents.js

--- src/components/GamingRoute/styledComponents.js
+++ src/components/GamingRoute/styledComponents.js
@@ -3,10 +3,14 @@ import Styled from 'styled-components'
 export const CombineContainer = Styled.div`
 display: flex;`
 
-export const UnorderedList = Styled.ul`
+export const UnorderedList = Styled.div`
 display: flex;
-flex-wrap: wrap;
+flex-direction: column;
 width: 80%;
 height: 75vh;
 overflow: scroll;
 background-color: ${props => (props.colorChange ? '#0f0f0f' : '#f9f9f9')};`
+
+export const GC = Styled.ul`
+display: flex;
+flex-wrap: wrap;`
