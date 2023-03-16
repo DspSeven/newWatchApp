84b1584883362273c43399365bb590e762ac5b78
DspSeven <119715565+DspSeven@users.noreply.github.com>
2023-03-16 12:30:22 +0530

    Update craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -1,1 +1,12 @@
-craco.config.js
+module.exports = {
+  babel: {
+    plugins: [
+      [
+        'babel-plugin-styled-components',
+        {
+          fileName: false,
+        },
+      ],
+    ],
+  },
+}
